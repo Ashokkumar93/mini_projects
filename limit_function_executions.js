dcf778e1e78c65621d417e73c0d2cab24188aaf3
Sai Gautham Peesapati <saigpeesapati@gmail.com>
2017-03-17 11:23:02 -0700

    A program with a function that limits the execution of a function to 2 times

--- limit_function_executions.js
+++ limit_function_executions.js
@@ -2,6 +2,8 @@ console.log(once(sum(2,3)));
 console.log(once(sum(1,10)));
 console.log(once(sum(1,10)));
 
+// A function that is limited in the number of times that it can be executed
+
 function once(x) {
     
     if (this.done == undefined) {
